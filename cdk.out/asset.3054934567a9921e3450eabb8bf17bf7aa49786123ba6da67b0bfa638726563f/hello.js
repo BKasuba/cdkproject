3828027139c5a9057101d00fda51de8d88e92589
Ben <bendeguz.kasuba@gmail.com>
2022-07-05 16:32:32 +0100

    Configured API Gateway to use lambda

--- cdk.out/asset.3054934567a9921e3450eabb8bf17bf7aa49786123ba6da67b0bfa638726563f/hello.js
+++ cdk.out/asset.3054934567a9921e3450eabb8bf17bf7aa49786123ba6da67b0bfa638726563f/hello.js
@@ -4,6 +4,6 @@
 exports.main = async function(event, context) {
     return{
         statusCode: 200,
-        body: 'hello lambda'
+        body: 'Hello from Lambda!'
     }
 }
--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,18 +1,32 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Code, Function as LambdaFunction, Runtime} from 'aws-cdk-lib/aws-lambda'
-import { join} from 'path';
+import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda'
+import { join } from 'path';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
 
 
 export class SpaceStack extends Stack{
+
+    private api = new RestApi(this,'SpaceApi')
+
     constructor(scope: Construct,id: string, props: StackProps){
         super(scope,id,props)
+
+
+
         //Below is what maps the lambda itself to the function which will have the logic in it. It points to the main function in the hello.js file
         const helloLambda = new LambdaFunction(this, 'helloLambda',{
             runtime: Runtime.NODEJS_16_X,
             code: Code.fromAsset(join(__dirname,'..','Services','Hello')),
             handler: 'hello.main'
         })
+
+        //Hello api lambda integration
+        const helloLambdaIntegration = new LambdaIntegration(helloLambda)
+        const helloLambdaResource = this.api.root.addResource('hello');
+        helloLambdaResource.addMethod('GET', helloLambdaIntegration)
+
+
     }
 
 
