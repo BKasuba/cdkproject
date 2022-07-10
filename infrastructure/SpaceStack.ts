d81289a6a0976868176aa4e81e0184f9e66db6b0
Ben <bendeguz.kasuba@gmail.com>
2022-07-10 12:33:24 +0100

    turned off lambda function, replaced with NodeJs function

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -23,19 +23,20 @@ export class SpaceStack extends Stack{
 
 
         //Below is what maps the lambda itself to the function which will have the logic in it. It points to the main function in the hello.js file
-        const helloLambda = new LambdaFunction(this, 'helloLambda',{
+        //Not used anymore 
+/*         const helloLambda = new LambdaFunction(this, 'helloLambda',{
             runtime: Runtime.NODEJS_16_X,
             code: Code.fromAsset(join(__dirname,'..','Services','Hello')),
             handler: 'hello.main'
-        })
+        }) */
 
         const helloLambdaNodeJs = new NodejsFunction(this,'helloLambdaNodeJs', {
             entry: (join(__dirname,'..','Services','Node-lambda','hello.ts')),//points to the file with implementation
             handler: 'handler'
         })
 
-        //Hello api lambda integration, the integration calls the helloLambda defined above
-        const helloLambdaIntegration = new LambdaIntegration(helloLambda)
+        //Hello api lambda integration, the integration calls the helloLambdaNodeJs defined above
+        const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs)
         const helloLambdaResource = this.api.root.addResource('hello');
         helloLambdaResource.addMethod('GET', helloLambdaIntegration)
 
